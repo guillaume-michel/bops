2c0fb0481f14a885df4380df9dd493f103bdb484
Guillaume MICHEL <guillaume.michel@orilla.fr>
2019-05-24 16:53:19 +0200

    WIP: change use of %array-data-vector by %array-data
    as it is specified in source code that the former is not to be used in
    new code.
    In code, the 2 functions are the same.

--- src/array-utils.lisp
+++ src/array-utils.lisp
@@ -3,7 +3,7 @@
 (defun simple-array-vector (array)
   (declare (simple-array array))
   (if (sb-kernel:array-header-p array)
-      (sb-kernel:%array-data-vector array)
+      (sb-kernel:%array-data array)
       array))
 
 (defun make-random-bit-vector (dims &key (probability-one 0.5f0))
